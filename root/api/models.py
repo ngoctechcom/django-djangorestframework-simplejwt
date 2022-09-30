3571907a823bc251db9a32b91bbfd526827699ea
Ngoc <ngocbthe@gmail.com>
2022-09-30 15:04:18 +0700

    Add project model

--- root/api/models.py
+++ root/api/models.py
@@ -1,3 +1,9 @@
+from pydoc import describe
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    describe = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
--- root/api/serializers.py
+++ root/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Project
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,16 @@ class UserSerializer(serializers.ModelSerializer):
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True)
+
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+class ProjectViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
