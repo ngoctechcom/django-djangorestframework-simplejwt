f097717f44661c47aa8b8e70342680416b79e699
Ngoc <ngocbthe@gmail.com>
2022-09-30 15:10:59 +0700

    add router

--- root/api/urls.py
+++ root/api/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import GetToken, Test
 from .serializers import ProjectViewSet
+from rest_framework import routers
+router = routers.SimpleRouter()
+router.register(r'projects', ProjectViewSet)
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/custom', GetToken.as_view(), name="token_custom"),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('hello/', Test.as_view(), name='hello'),
-    path('project/', ProjectViewSet.as_view(), name='project')
 ]
+urlpatterns += router.urls
